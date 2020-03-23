17e32c876624e743a020d90a0de5d75987123987
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-23 14:47:09 -0400

    change Milk, Bread, Price using regex. Trying to see if I can use Jackson JSON Java Parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParseException;
 import org.apache.commons.io.IOUtils;
 
+import javax.swing.text.html.parser.Parser;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -15,8 +20,8 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
-
+        dataParser groceryItems = new dataParser();
+        groceryItems.displayGroceryItems();
+        System.out.println(groceryItems);
     }
-
 }
--- src/main/java/dataParser.java
+++ src/main/java/dataParser.java
@@ -1,28 +1,36 @@
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class dataParser {
-    private String dataParser;
-    public void  DataParser() throws IOException {this.dataParser = newFile();}
+class dataParser<rootNode> {
+    private String dataInfo;
+
+    public dataParser() throws IOException {
+        this.dataInfo = newFile();
+    }
 
     public static String newFile() throws IOException {
 
         //create JsonParser object
 
         //read  file data to String
-        //File file = new File("/Users/ahoward/Desktop/projects/HurtLocker/target/classes/RawData.txt");
-        JsonParser jsonParser = new JsonFactory().createParser(new File("/Users/ahoward/Desktop/projects/HurtLocker/target/classes/RawData.txt"));
+        File file = new File("/Users/ahoward/Desktop/projects/HurtLocker/target/classes/RawData.txt");
+        //JsonParser jsonParser = new JsonFactory().createParser(new File("/Users/ahoward/Desktop/projects/HurtLocker/target/classes/RawData.txt"));
         StringBuilder result = new StringBuilder("");
 
-        try (Scanner scanner = new Scanner((Readable) jsonParser)) {
+        try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
@@ -33,23 +41,77 @@ class dataParser {
     }
 
     public String getDataParser() {
-        return dataParser;
+        return dataInfo;
     }
-    public String changeNameToLowerCase(String str) {
+
+    public void displayGroceryItems() {
+        changeNameToLowerCase();
+        removeHashNewLine();
+        replaceMilkMilk();
+        replaceBread();
+        replacePrice();
+    }
+
+    public String changeNameToLowerCase() {
 
         Pattern pattern = Pattern.compile("naMe", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(str.toLowerCase());
+        Matcher matcher = pattern.matcher(dataInfo);
 
-        return matcher.replaceAll("naMe");
+        return matcher.replaceAll("name");
     }
 
-    public String removeHashNewLine(String str) {
+    public String removeHashNewLine() {
 
         Pattern pattern = Pattern.compile("[##]", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(str);
+        Matcher matcher = pattern.matcher(dataInfo);
         return matcher.replaceAll("\n");
     }
-}
+
+    public String replaceMilkMilk() {
+
+        Pattern pattern = Pattern.compile("milk", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(dataInfo);
+        return matcher.replaceAll("Milk");
+    }
+
+    public String replaceBread() {
+        Pattern pattern = Pattern.compile("bread", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(dataInfo);
+        return matcher.replaceAll("Bread");
+    }
+
+    public void replacePrice() {
+        int count = 0;
+        Pattern pattern = Pattern.compile("price", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(dataInfo);
+
+        while(matcher.find()) {
+            count++;
+        }
+
+        System.out.println("Price:   1.23        seen: " + count + " " + "times");
+    }
+    /*
+    byte[] jsonData = Files.readAllBytes(Paths.get("RawData.txt"));
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    //create JsonNode
+    JsonNode rootNode = objectMapper.readTree(jsonData);
+
+
+//update JSON data
+((dataParser) rootNode).put("id", 500);
+//add new key value
+((ObjectNode) rootNode).put("test", "test value");
+//remove existing key
+((ObjectNode) rootNode).remove("role");
+((ObjectNode) rootNode).remove("properties");
+objectMapper.writeValue(new File("updated_emp.txt"), rootNode);
+
+
+     */
+    }
 
 /*
     public class HamletParser {
